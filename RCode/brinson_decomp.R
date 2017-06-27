a3d04c9881515a2e001f158aff444edcfe033f69
Ruiling <fengruiling@outlook.com>
2017-06-27 09:09:10 +0800

    check internal rating between CORE & DW

--- RCode/brinson_decomp.R
+++ RCode/brinson_decomp.R
@@ -1,32 +1,31 @@
-lapply(list("GCAMCPUB", "data.table", "dplyr", "portanalytics"), library, ch = TRUE)
+# lapply(list("GCAMCPUB", "data.table", "portanalytics"), library, ch = TRUE)
+library(GCAMCPUB)
+library(data.table)
+library(portanalytics)
 #see more from dataDef.R
-#basic data configuation: load all data from cost based and unit linked from 2014-12 to 2016-04
-portanalytics::paDM$setLdcPath("E:/paData")
-cbstr <- c("CNPC", "Par", "Life", "UV Individual", "UV Group", "CNPC-UV")
-ulstr <- c("UL Anyi", "UL Aggressive", "UL Stable", "UL Increase", "UL Strategy", 
-           "Mighty Rotation", "Flexible Allocation", "Daily Increase")
-portanalytics::paDM$readFromLdc(c("201412", "201604"), c(cbstr, ulstr))
-dataCenter <- portanalytics::paDM$dataCenter
-#Brinson function
-Brinson_decomp <- function(Input_Port, Date_From, Date_To){
+
+readFromLdc(c("201512", "201706"), c("Unit linked", "Third party"),
+            jyDateRange = NULL)
+
+# paras -------------------------------------------------------------------
+
+pn <- "UL Aggressive"
+date_from <- as.Date("2015-12-31")
+date_to <- as.Date("2016-12-31")
+
+# define the function -----------------------------------------------------
+
+brinson_decomp <- function(pn, date_from, date_to){
   ##match table preparation
   bchmrkw <- data.table(c("UL Stable", "UL Anyi", "UL Increase",
                           "UL Strategy", "UL Aggressive"),
-                        c(0.05, 0.05, 0.5, 0.5, 0.8),
-                        c(0.9, 0.9, 0.45, 0.45, 0.15),
+                        c(0.05, 0.05, 0.4, 0.4, 0.8),
+                        c(0.9, 0.9, 0.55, 0.55, 0.15),
                         rep(0.05,5)) %>%
     setnames(c("Port_Name", "Equity_w", "FI_w", "Liquidity_w"))
   bchmk_match <-  data.table(c("Bond", "Equity", "Liquidity"),
-                             c("0571.CS", "000300.SH", "R007.IB")) %>%
+                             c("0571.CS", "000300.SH", "Cash")) %>%
     setnames(c("Class_L1", "Bchmk_Code"))
-  Name_Codematch <- data.table(c(rep("UL Stable", 4), rep("UL Anyi", 3), rep("UL Increase", 4),
-                                 rep("UL Strategy", 3), rep("UL Aggressive", 3)), 
-                               c(portanalytics::portSelector(x = 'UL Stable', portType = "HS_Port_Code"),
-                                 portanalytics::portSelector(x = 'UL Anyi', portType = "HS_Port_Code"),
-                                 portanalytics::portSelector(x = 'UL Increase', portType = "HS_Port_Code"),
-                                 portanalytics::portSelector(x = 'UL Strategy', portType = "HS_Port_Code"),
-                                 portanalytics::portSelector(x = 'UL Aggressive', portType = "HS_Port_Code"))) %>%
-    setnames(c("Port_Name", "HS_Port_Code"))
   Class_match <- data.table(c(portanalytics::astClassSelector("Equity"), portanalytics::astClassSelector(
     "Fixed income"), portanalytics::astClassSelector("Liquidity")),
     c(rep("Equity", length(portanalytics::astClassSelector("Equity"))),
@@ -35,22 +34,22 @@ Brinson_decomp <- function(Input_Port, Date_From, Date_To){
     setnames(c("Class_L3_", "Class_L1"))
   Class_match[Class_L1 == 'Fixed income', Class_L1 := 'Bond']
   # data preparation from dataCenter & paModel$unitPrice
-  bchmk <- dataCenter$bchmk[Bchmk_Code %in% c('0571.CS', '000300.SH', "R007.IB") &
+  bchmk <- dataCenter$bchmk[Bchmk_Code %in% c('0571.CS', '000300.SH', "Cash") &
                               Date >= Date_From & Date <= Date_To, ]
   bchmk <- bchmk_match[bchmk, on = "Bchmk_Code"]
-  pos <- dataCenter$pos[Date >= Date_From & Date <= Date_To, ] %>%
-    select(Date, HS_Port_Code, AV_Mix_LC, Class_L3_)
-  pos <- Name_Codematch[pos, on = "HS_Port_Code"]
+  pos <- dataCenter$pos[Date >= Date_From & Date <= Date_To, ]
+  pos <- pos[, .(Date, HS_Port_Code, AV_Mix_LC, Class_L3_)]
+  pos <- attachHSPortInfo(pos, "Port_Name")
   pos <- pos[Port_Name == Input_Port, ]
   #how to use list to read uniked linked data once time? is it necessary?
-  unitPrice <- portanalytics::paModel$unitPrice(Input_Port, 
-                                                dates = as.Date(c(Date_From, Date_To)))
+  unitPrice <- portanalytics::unitPrice(Input_Port, 
+                                        dates = as.Date(c(Date_From, Date_To)))
   #Portfolio return
   Return_port <- last(unitPrice$Cum_Rtn)
   #cf & pos aggregation by Class_L1 and combine pos & cf
-  cf <- dataCenter$trans[Date >= Date_From & Date <= Date_To, ] %>%
-    select(Date, HS_Port_Code, Class_L3_, Trans_Type, CF_LC)
-  cf <- Name_Codematch[cf, on = "HS_Port_Code"]
+  cf <- dataCenter$trans[Date >= Date_From & Date <= Date_To, ]
+  cf <- cf[, .(Date, HS_Port_Code, Class_L3_, Trans_Type, CF_LC)]
+  cf <- attachHSPortInfo(cf, "Port_Name")
   cf <- cf[Port_Name == Input_Port, ]
   # change the weedends' cf to the next monday
   cf <-
@@ -61,17 +60,22 @@ Brinson_decomp <- function(Input_Port, Date_From, Date_To){
       tmp[, Date := NULL]
       data.table::setnames(tmp, "DateNew", "Date")
     })
-  cf <- mutate(cf, CFpos = ifelse(CF_LC > 0, CF_LC, 0),
-               CFneg = ifelse(CF_LC < 0, -CF_LC, 0))
-  cf_sum <- cf[, list(cf_pos = sum(CFpos), cf_neg = sum(CFneg)), by = .(Date, Class_L3_)]
+  cf[, `:=`(
+    CF_Pos = ifelse(CF_LC > 0, CF_LC, 0),
+    CF_Neg = ifelse(CF_LC < 0, -CF_LC, 0)
+  )]
+  cf_sum <- cf[, list(CF_Pos = sum(CF_Pos), CF_Neg = sum(CF_Neg)), by = .(Date, Class_L3_)]
   cf_sum <- Class_match[cf_sum, on = 'Class_L3_']
-  cf_sum <- cf_sum[, list(cf_pos = sum(cf_pos), cf_neg = sum(cf_neg)), by = .(Date, Class_L1)]
+  cf_sum <- cf_sum[, list(CF_Pos = sum(CF_Pos), CF_Neg = sum(CF_Neg)), by = .(Date, Class_L1)]
   pos <- Class_match[pos, on = 'Class_L3_']
   pos <- pos[, list(AV_Mix_LC = sum(abs(AV_Mix_LC))), by = .(Date, Port_Name, Class_L1)]
-  pos <- cf_sum[pos, on = c("Date", "Class_L1")] %>%
-    mutate(cf_pos = ifelse(is.na(cf_pos), 0.0, cf_pos), 
-           cf_neg = ifelse(is.na(cf_neg), 0.0, cf_neg)) %>%
-    arrange(Date)
+  pos <- cf_sum[pos, on = c("Date", "Class_L1")]
+  pos <- pos[, `:=`(
+    CF_Pos = ifelse(is.na(CF_Pos), 0.0, CF_Pos), 
+    CF_Neg = ifelse(is.na(CF_Neg), 0.0, CF_Neg) 
+  )] %>%
+    setorder(Date)
+  
   ##w_bchmk
   w_bchmk <-
     local({
@@ -83,9 +87,11 @@ Brinson_decomp <- function(Input_Port, Date_From, Date_To){
   bchmk <- w_bchmk[bchmk, on = 'Class_L1']
   
   #compute wp, DR_p, wb, DR_b, note the DR of liquidity is R /365
+  setDT(pos)
   pos[, wp := AV_Mix_LC / sum(AV_Mix_LC), by = Date]
-  pos <- pos[, DR_p := (AV_Mix_LC + cf_pos) / (data.table::shift(AV_Mix_LC, 1L) + cf_neg) - 1, by = Class_L1] %>%
-    mutate(DR_p = GCAMCPUB::na_fill(DR_p, 0.0))
+  pos <- pos[, DR_p := na_fill(
+    (AV_Mix_LC + CF_Pos) / (data.table::shift(AV_Mix_LC, 1L) + CF_Neg) - 1, 0.0),
+    by = Class_L1] 
   bchmk[, DR_b := Bchmk_Value / shift(Bchmk_Value, 1L) - 1, by = .(Class_L1)]
   bchmk[Class_L1 == 'Liquidity', DR_b := Bchmk_Value / 365 / 100]
   bchmk[Date == Date_From & Class_L1 == 'Liquidity', DR_b := 0.0]
@@ -93,14 +99,20 @@ Brinson_decomp <- function(Input_Port, Date_From, Date_To){
   pos <- bchmk[pos, on = c("Date", "Class_L1")]
   pos <- w_bchmk[pos, on = 'Class_L1']
   pos[Class_L1 == "Liquidity", DR_p := DR_b]
-  pos <- mutate(pos, deta_DR = DR_p - DR_b, deta_w = wp - wb)
+  pos[, `:=`(
+    deta_DR = DR_p - DR_b,
+    deta_w = wp - wb
+  )]
   bchmk <- bchmk[, list(DR = sum(DR_b * wb)), by = .(Date)]
   bchmk <- bchmk[, RTN := cumprod(DR + 1) - 1]
   Return_bchmk <- last(bchmk$RTN)
-  pos <- pos[, list(deta_w_Rb = sum(deta_w * DR_b), wb_deta_DR = sum(wb * deta_DR)), by = Date]
-  pos <- pos[, AllocationEffect := cumprod(deta_w_Rb + 1) - 1]
-  pos <- pos[, SecuritySelection := cumprod(wb_deta_DR + 1) - 1]
-  
+  setDT(pos)
+  pos <- pos[, list(deta_w_Rb = sum(deta_w * DR_b), 
+                    wb_deta_DR = sum(wb * deta_DR),
+                    other = sum(deta_w * deta_DR)), by = Date]
+  pos[, AllocationEffect := cumprod(deta_w_Rb + 1) - 1]
+  pos[, SecuritySelection := cumprod(wb_deta_DR + 1) - 1]
+  pos[, Other := cumprod(other + 1) - 1]
   #total Benchmark return
   
   # show results
@@ -112,12 +124,18 @@ Brinson_decomp <- function(Input_Port, Date_From, Date_To){
                         AllocationEffect, SecuritySelection, Others))) %>%
     setnames(c("Port_Name", "Return_port", "Return_bchmk",  "Return_excess",
                "AllocationEffect", "SecuritySelection", "Others"))
-  result <- data.table(t(c(Input_Port, f_fmt_pct(as.numeric(select(tmp, -Port_Name)[1, ]))))) %>%
-    setnames(names(tmp))
-  result
+  tmp[, `:=`(
+    Return_Port = f_fmt_pct(as.numeric(Return_port)),
+    Return_Bchmk = f_fmt_pct(as.numeric(Return_bchmk)),
+    Return_Excess= f_fmt_pct(as.numeric(Return_excess)),
+    Alloc = f_fmt_pct(as.numeric(AllocationEffect)),
+    Select = f_fmt_pct(as.numeric(SecuritySelection)),
+    Others = f_fmt_pct(as.numeric(Others))
+  )]
+  tmp <- tmp[, .(Port_Name, Return_Port, Return_Bchmk, Return_Excess, Alloc, Select, Others)]
 }
 #test brinson
-Brinson_decomp("UL Stable", "2014-12-31", "2015-11-23")
+Brinson_decomp("UL Aggressive", "2015-12-31", "2016-12-31")
 
 
 #Dietz function
@@ -143,4 +161,4 @@ Brinson_decomp("UL Stable", "2014-12-31", "2015-11-23")
 
 
 
-#test dietz
+
---
+++ RCode/check_rating.R
@@ -0,0 +1,113 @@
+# The code is used for regularly check the internal rating between CORE System and DW warehouse.
+# Since the DW data coming from HengTai rating system, so the rating update will be more timely.
+# amdw.v_dw_rating_i is a view, amdw.dw_rating_i is a real table
+
+# NOTE!!!!!!!!!
+# the rating of non-standard products from DW are wrong, which should be told to IT if having time.
+
+# pkgs --------------------------------------------------------------------
+
+library(GCAMCPUB)
+library(DBI)
+library(data.table)
+
+# paras -------------------------------------------------------------------
+
+tgt_date <- as.Date("2017-11-30")
+# fetch rating data from oracle
+get_dw <- (function(){
+  db_dw <- activate_conn("db_dw")
+  on.exit(DBI::dbDisconnect(db_dw))
+  # get the internal rating (incl. bond & issuer)
+  sql <- "Select * from amdw.dw_rating_i where to_date(BDATE, 'YYYY-MM-DD') <= date'{{date}}' and 
+  to_date(EDATE, 'YYYY-MM-DD') > date'{{date}}'"
+  sql <- infuser::infuse(sql, date = tgt_date)
+  internal <- dbGetQuery(db_dw, sql) %>%
+    setDT()
+  internal[, `:=`(
+    EDATE = to_date(EDATE),
+    BDATE = to_date(BDATE)
+  )]
+  internal_bond <- internal[R_TYPE == "BI", .(TCODE, TNAME, CR, ISSUER_NAME, ISSUER_CODE,
+                                              BDATE, EDATE)] %>%
+    setnames("TCODE", "Sec_Code")
+  internal_bond[, Tgt_Date := BDATE]
+  
+  internal_bond_unique <- unique(internal_bond[, .(Sec_Code, TNAME, ISSUER_NAME,
+                                                  ISSUER_CODE)])
+  internal_bond_unique[, Tgt_Date := tgt_date]
+  
+  internal_bond <- internal_bond[internal_bond_unique, on = c("Sec_Code", "TNAME", "ISSUER_NAME",
+                                                     "ISSUER_CODE", "Tgt_Date"),
+                                 roll = TRUE, mult = "first"]
+  
+  internal_issuer <- internal[R_TYPE == "EI", .(TCODE, TNAME, CR, BDATE, EDATE)] %>%
+    setnames("TNAME", "Issuer")
+  internal_issuer[, Tgt_Date := BDATE]
+  internal_issuer_unique <- unique(internal_issuer[, .(Issuer)])
+  internal_issuer_unique[, Tgt_Date := tgt_date]
+  internal_issuer <- internal_issuer[internal_issuer_unique, on = c("Issuer", "Tgt_Date"),
+                                     roll = TRUE, mult = "first"]
+  
+  mget(c("internal_bond", "internal_issuer"))
+})()
+
+# fetch rating data from CORE
+core_rating <- (function(){
+  db_mo <- activate_conn(db_mo)
+  on.exit(DBI::dbDisconnect(db_mo))
+  sql <- "
+  select Date, Sec_Code, Sec_Name, Class_L3, Rating_Internal, 
+  Issuer, Issuer_Rating_Internal, HS_Port_Code from CORE_Data_Holding
+  where date = '{{date}}'
+  and Class_L1 = 'fixed income' and Class_L3  not in ('bond fund', 'IAMP-FI-NAV', 'IAMP-FI') "
+  sql <- infuser::infuse(sql, date = tgt_date)
+  core_rating <- dbGetQuery(db_mo, sql) %>%
+    setDT()
+  core_rating[, Date := to_date(Date)]
+  core_rating
+})()
+
+fc_port <- (function(){
+  db_mo <- activate_conn(db_mo)
+  on.exit(DBI::dbDisconnect(db_mo))
+  sql <- "
+  select distinct HS_Port_Code from CORE_para_Port where if_FC_HS_Port = 1"
+  fc_port <- dbGetQuery(db_mo, sql)$HS_Port_Code
+  fc_port
+})()
+
+# comparison of bond rating
+check_bond <- get_dw$internal_bond[core_rating, on = "Sec_Code"][, .(
+  Sec_Code, TNAME, Sec_Name, Class_L3, CR, Rating_Internal, BDATE, EDATE
+)]
+dw_null <- check_bond[is.na(TNAME) & is.na(CR)]
+inconsis <- check_bond[!is.na(CR) & CR != Rating_Internal & 
+                         ! (CR == "risk free" & Rating_Internal == "Risk_free")]
+write_open_xlsx(unique(inconsis))
+
+
+# Note: HTZNTD.IB, TKQJNY.IB
+# there are 2 or even more records in dw database due to some wrong type-in issuers.
+# The credit AA in our holding is right.
+
+
+
+# comparison of issuer rating
+
+# pick the null issuer and look it up in dw
+issuer_null <- core_rating[is.na(Issuer) & ! Class_L3 %in% c("ABS", "CD") &
+                             ! HS_Port_Code %in% fc_port]
+print(issuer_null)
+check_issuer <- get_dw$internal_issuer[core_rating, on = "Issuer"][, .(
+  Issuer, Sec_Code, Sec_Name, CR, Issuer_Rating_Internal, BDATE, EDATE
+)]
+check_issuer <- check_issuer[!is.na(Issuer)]
+
+
+
+
+# match the internal bond with issuer
+issuer_code <- unique(internal_issuer[, .(ISSUER_CODE, BDATE, EDATE, CR)])
+issuer_name <- unique(internal_issuer[, .(TNAME, BDATE, EDATE, CR)])
+internal
