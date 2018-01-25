d45736b93a5788c978c58d442dd731603ce20a91
Ruiling <fengruiling@outlook.com>
2018-01-25 18:16:59 +0800

    find the quantile rtn of a bunch of funds

---
+++ RCode/fund_quantile.R
@@ -0,0 +1,30 @@
+library(WindR)
+library(GCAMCPUB)
+library(data.table)
+library(dplyr)
+
+quantile_para <- 0.3
+fund_code <- c("000008.OF,000041.OF,000042.OF,000043.OF,000044.OF")
+
+# data reading ------------------------------------------------------------
+
+w.start()
+w_wsd_data<-w.wsd(fund_code,
+                  "NAV_adj_return1","2017-12-31","2018-01-24","Fill=Previous;PriceAdj=B")
+stopifnot(w_wsd_data$ErrorCode == 0L)
+data <- w_wsd_data$Data %>% setDT()
+
+
+f <- function(x) {
+  x <- cumprod(1 + x / 100) - 1
+}
+cum_rtn <- lapply(data[, -"DATETIME", with = FALSE], f) %>% setDT()
+
+cum_rtn <- cbind(data$DATETIME, cum_rtn) %>% setDT()
+
+# find quantile 
+quantile <- apply(cum_rtn[, -"V1", with = FALSE], 1, quantile, quantile_para)
+quantile <- data.table(Quantile = quantile)
+
+
+res <- cbind(cum_rtn, quantile)
