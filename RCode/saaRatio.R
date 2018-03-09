f29c7e2986718600fe1d7f97b6b5afc5acd0229b
Ruiling <fengruiling@outlook.com>
2018-03-09 17:04:22 +0800

    add saa ratio

---
+++ RCode/saaRatio.R
@@ -0,0 +1,27 @@
+library(portanalytics)
+library(GCAMCPUB)
+library(data.table)
+
+readFromLdc(c("201709", "201802"), "All cost based", jyDateRange = NULL)
+
+# Note: you can't use the class_relationship key by Class_L3, because bond and stock conatins
+# both domestic and overseas. You have to dive into the underlying pos for SAA proportion.
+
+tgt_date <- as.Date("2017-09-30")
+# exclude Capital FX
+# saa_alloc <-  astAlloc(paPorts(list(c("CNPC", "Par", "Life", "Capital RMB", "UV Individual", "UV Group")
+#                                    ), "Cost based"),
+#                       dates = tgt_date,
+#                       groupBy = "Class_L3") %>%
+#   setnames("Group", "Class_L3")
+
+
+saa_cl1 <- dataCenter$pos[Date == tgt_date, .(AV = sum(AV_Mix_LC)), by = .(SAA_CL1)]
+saa_cl1[, Ratio := AV / sum(AV)]
+
+saa_cl2 <- dataCenter$pos[Date == tgt_date, .(AV = sum(AV_Mix_LC)), by = .(SAA_CL2)]
+saa_cl2[, Ratio := AV / sum(AV)]
+
+
+
+
